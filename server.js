66140dd685fab1ec59553a1e5eca38a30e8252d7
GabrielPDB <39529079+GabrielPDB@users.noreply.github.com>
2020-10-30 22:34:46 -0300

    files reorganized

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const recipes = require("./data")
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noCache: true
+})
+
+server.get('/', function(req, res) {
+    return res.render('index')
+})
+
+server.get('/about', function(req, res) {
+    return res.render('about')
+})
+
+server.get('/recipes', function(req, res) {
+    return res.render('recipes')
+})
+
+server.listen(3000, function () {
+    console.log('server is running')
+})
