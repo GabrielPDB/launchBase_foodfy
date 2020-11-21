a42aa517575c16d45d408ca59923cff72de61a99
GabrielPDB <39529079+GabrielPDB@users.noreply.github.com>
2020-11-21 16:07:03 -0300

    routes.js and server.js ok

--- routes.js
+++ routes.js
@@ -1,5 +1,28 @@
 const express = require('express')
 const routes = express.Router()
+const recipes = require("./data")
+
+/* ROUTES WEBSITE */
+
+routes.get('/', function (req, res) {
+    return res.render('home', { recipes })
+})
+
+routes.get('/about', function (req, res) {
+    return res.render('about', { page: 'about' })
+})
+
+routes.get('/recipes', function (req, res) {
+    return res.render('recipes', { recipes, page: 'recipes' })
+})
+
+routes.get("/recipes/:index", function (req, res) {
+    const recipeIndex = req.params.index;
+
+    return res.render('recipe', { recipe: recipes[recipeIndex], page: 'recipes'} )
+})
+
+/* ROUTES ADMIN */
 
 /* routes.get("/admin/recipes", recipes.index)
 routes.get("/admin/recipes/create", recipes.create)
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 const routes = require('./routes')
 const methodOverride = require('method-override')
-const recipes = require("./data")
 
 const server = express()
 
@@ -20,24 +19,6 @@ nunjucks.configure('views', {
     express: server,
     autoescape: false,
     noCache: true
-})
-
-server.get('/', function (req, res) {
-    return res.render('home', { recipes })
-})
-
-server.get('/about', function (req, res) {
-    return res.render('about', { page: 'about' })
-})
-
-server.get('/recipes', function (req, res) {
-    return res.render('recipes', { recipes, page: 'recipes' })
-})
-
-server.get("/recipes/:index", function (req, res) {
-    const recipeIndex = req.params.index;
-
-    return res.render('recipe', { recipe: recipes[recipeIndex], page: 'recipes'} )
 })
 
 server.listen(3100, function () {
