1b3af3698f1a085a100aa1ddf9d986511ee99272
GabrielPDB <39529079+GabrielPDB@users.noreply.github.com>
2020-11-22 14:13:36 -0300

    dynamic inputs ok

--- public/scripts.js
+++ public/scripts.js
@@ -12,12 +12,48 @@ for (let card of cards) {
 
 const currentPage = location.pathname
 const menuItems = document.querySelectorAll('header .menu a')
-console.log(currentPage)
+
 for (item of menuItems) {
     if (currentPage.includes(item.getAttribute('href'))) {
         item.classList.add('selected')
     }
 }
+
+/* DYNAMIC INPUTS */
+
+function addIngredient() {
+    const ingredients = document.querySelector('#ingredients')
+    const fieldContainer = document.querySelectorAll('.ingredient')
+
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == '') return false
+    
+    // Deixa o valor do input vazio
+    newField.children[0].value = ''
+    ingredients.appendChild(newField)
+}
+
+function addStep() {
+    const steps = document.querySelector('#steps')
+    const fieldContainer = document.querySelectorAll('.step')
+
+
+    // Realiza um clone do último passo adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == '') return false
+
+    // Deixa o valor do input vazio
+    newField.children[0].value = ''
+    steps.appendChild(newField)
+}
+
+document.querySelector('.add-ingredient').addEventListener('click', addIngredient)
+document.querySelector('.add-step').addEventListener('click', addStep)
 
 /* RECIPE SHOW/HIDE */
 
@@ -30,7 +66,7 @@ for (let button of buttons) {
     })
 }
 
-for (let button in buttons) {
+for (const button in buttons) {
     buttons[button].addEventListener('click', function () {
         if (buttons[button].innerHTML === "esconder") {
             buttons[button].innerHTML = 'mostrar'
